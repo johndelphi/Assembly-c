7664d8c09b4390e821f128f98c969de54b3aed48
John.michael <John.michael@studentambassadors.com>
2021-02-14 21:58:42 -0600

    project done

---
+++ Source.asm
@@ -0,0 +1,21 @@
+; AddTwoSum_64.asm - Chapter 3 example.
+; Student Name (John muema michael)
+; Computer Organization
+; COSC 2325-2801
+; Date 1/28/2021
+ExitProcess proto
+
+.data
+sum qword 0
+
+.code
+main proc
+	mov	rax,5
+	add	rax,6
+	mov   sum,rax
+
+	mov   ecx,0
+	call  ExitProcess
+
+main endp
+end
---
+++ chapter2registers.asm
@@ -0,0 +1,28 @@
+; AddTwoSum_64.asm - Chapter 3 example.
+; Student Name (John muema michael)
+; Computer Organization
+; COSC 2325-2801
+; Date 2/14/2021
+ExitProcess proto
+;description
+using registers: A = (A + B) + (C + D). Assign integer values to the EAX, EBX, ECX, and EDX registers
+
+.data
+A qword 5 ; where we store our  A value
+B qword 7 ; where we store our B value
+c qword 8 ; where we store our B value
+D qword 3 ; where we store the value for D
+sum qword A ; where we will move the total  sum of all the numbers 
+
+.code
+main proc
+	mov	rax,A   ; moving the value of first num to the rax register
+	add	rax,B   ; adding and moving the value of (A+b) to rax
+	add rax,C   ; adding the value comtained in C to the value of rax
+	add rax,D   ; adding D to rax and then moving  the total  to rax register
+	mov   sum,rax ; moving the total of (A+B+C+D) to sum 
+	mov   ecx,0
+	call  ExitProcess
+
+main endp
+end
